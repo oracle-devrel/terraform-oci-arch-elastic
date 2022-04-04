89ebd72a998e147c1e3f8eb3e4829571701e1371
Nuno Gonçalves <nuno.goncalves@oracle.com>
2022-04-04 17:42:10 +0100

    restructuring folders

---
+++ simple/orm/variables.tf
@@ -0,0 +1,102 @@
+## Copyright (c) 2022, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+variable "tenancy_ocid" {}
+#variable "user_ocid" {}
+#variable "fingerprint" {}
+#variable "private_key_path" {}
+variable "region" {}
+variable "compartment_ocid" {}
+variable "availability_domain_name" {
+  default = ""
+}
+variable "availability_domain_number" {
+  default = 0
+}
+
+variable "release" {
+  description = "Reference Architecture Release (OCI Architecture Center)"
+  default     = "2.1"
+}
+
+variable "ssh_public_key" {
+  default = ""
+}
+
+variable "VCN-CIDR" {
+  default = "10.1.0.0/16"
+}
+
+variable "ELKSubnet-CIDR" {
+  default = "10.1.20.0/24"
+}
+
+variable "instance_shape" {
+  default = "VM.Standard.A1.Flex"
+}
+
+variable "instance_flex_shape_ocpus" {
+    default = 1
+}
+
+variable "instance_flex_shape_memory" {
+    default = 15
+}
+
+variable "instance_os" {
+  description = "Operating system for compute instances"
+  default     = "Oracle Linux"
+}
+
+variable "linux_os_version" {
+  description = "Operating system version for all Linux instances"
+  default     = "8"
+}
+
+variable "elasticsearch_download_url" {
+  default = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch"
+}
+
+variable "elasticsearch_download_version" {
+  default = "7.16.3"
+}
+
+variable "kibana_download_url" {
+  default = "https://artifacts.elastic.co/downloads/kibana/kibana"
+}
+
+variable "kibana_download_version" {
+  default = "7.16.3"
+}
+
+variable "logstash_download_url" {
+  default = "https://artifacts.elastic.co/downloads/logstash/logstash"
+}
+
+variable "logstash_download_version" {
+  default = "7.16.3"
+}
+
+variable "KibanaPort" {
+  default = "5601"
+}
+
+variable "ESDataPort" {
+  default = "9200"
+}
+
+# Dictionary Locals
+locals {
+  compute_flexible_shapes = [
+    "VM.Standard.E3.Flex",
+    "VM.Standard.E4.Flex",
+    "VM.Standard.A1.Flex"
+  ]
+}
+
+# Checks if is using Flexible Compute Shapes
+locals {
+  is_flexible_shape = contains(local.compute_flexible_shapes, var.instance_shape)
+}
+
+
